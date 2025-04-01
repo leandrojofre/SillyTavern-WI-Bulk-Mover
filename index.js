09635b19572b640d7084327554fb29259700870a
Leandro Jofré <leandrotomasjofre@gmail.com>
2025-04-01 04:47:17 -0300

    Initialize staging

---
+++ index.js
@@ -0,0 +1,97 @@
+import {extension_settings} from "../../../extensions.js";
+import {saveSettingsDebounced, event_types, eventSource} from "../../../../script.js";
+import {getLocalVariable, getGlobalVariable} from "../../../variables.js";
+
+// * Extension variables
+
+const extensionName = "SillyTavern-Extension-Template";
+const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {
+    enabled: true,
+    debug: false
+};
+
+const context = SillyTavern.getContext();
+
+// * Debugs methods
+
+const log = (...msg) => {
+    if (!extensionSettings.enabled || !extensionSettings.debug) return;
+    console.log("[" + extensionName + "]", ...msg);
+};
+
+// * Extension methods
+
+// ...
+
+// * Methods in charge of controlling the extension settings
+
+const settingsCallbacks = {
+    /**	Triggers on enabled setting change. */
+    enabled: () => {
+        // Nothing by the moment
+    }
+}
+
+/** Changes a setting value and triggers a callback if there's any on settingsCallbacks. */
+function settingsBooleanButton(event) {
+    const target = event.target;
+    const value = Boolean($(target).prop("checked"));
+    const setting = target.getAttribute("EXTENSION_NAME-setting");
+    const callback = settingsCallbacks[setting];
+
+    extensionSettings[setting] = value;
+
+    if (callback) callback();
+
+    log("toggleSetting " + setting, value);
+    saveSettingsDebounced();
+}
+
+/**	Logs setting's values. */
+function displaySettings() {
+    console.debug("[" + extensionName + "]", `The extension is ${extensionSettings.enabled ? "active" : "not active"}`);
+    console.debug("[" + extensionName + "]", `Debug mode is ${extensionSettings.debug ? "active" : "not active"}`);
+    console.debug("[" + extensionName + "]", structuredClone(extensionSettings));
+}
+
+/** Append settings menu on ST and set listeners. */
+async function loadHTMLSettings() {
+    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
+
+    $("#extensions_settings").append(settingsHtml);
+
+    // Event Listeners for the extension HTML
+    $("#EXTENSION_NAME-activate-extension").on("input", settingsBooleanButton);
+    $("#EXTENSION_NAME-activate-debug").on("input", settingsBooleanButton);
+    $("#EXTENSION_NAME-check-configuration").on("click", displaySettings);
+
+    log("loadHTMLSettings");
+}
+
+/** Init setting values on the menu */
+function setSettings() {
+    $("#EXTENSION_NAME-activate-extension").prop("checked", extensionSettings.enabled).trigger("input");
+    $("#EXTENSION_NAME-activate-debug").prop("checked", extensionSettings.debug).trigger("input");
+
+    log("setSettings", extensionSettings);
+}
+
+// * Initialize Extension
+
+(async function initExtension() {
+
+    if (!context.extensionSettings[extensionName]) {
+        context.extensionSettings[extensionName] = structuredClone(defaultSettings);
+    }
+
+    for (const key of Object.keys(defaultSettings)) {
+        if (context.extensionSettings[extensionName][key] === undefined) {
+            context.extensionSettings[extensionName][key] = defaultSettings[key];
+        }
+    }
+
+    await loadHTMLSettings();
+    setSettings();
+})();
